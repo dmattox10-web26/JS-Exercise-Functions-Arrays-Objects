eda056d8895df7c4a95ce9cafa714834fed57116
Daniel Mattox <dmattox10@gmail.com>
2019-11-09 18:43:09 -0500

    ninth function passing

--- index.js
+++ index.js
@@ -236,8 +236,14 @@ function getModelYears(inventory) {
  * with a `car_year` which is at most the given desired max year,
  * in the same order as they appear in the original inventory.
 */
-function getOlderCars(/* code here */) {
-  /* code here */
+function getOlderCars(inventory, year) {
+  let cars = []
+  for (let i = 0; i < inventory.length; i++) {
+    if (inventory[i].car_year <= year) {
+      cars.push(inventory[i])
+    }
+  }
+  return cars
 }
 
 /**
